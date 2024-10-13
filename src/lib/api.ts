31eabdbdac3b29fe7c9863c9cadc5f42904632ed
LaPepega <tmv03092008@gmail.com>
2024-10-13 20:34:40 +0500

    Simple schedule

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,1 +1,60 @@
-// Functions for api calls to the dnevnik
+// Functions and interfaces for api calls to the dnevnik
+
+interface Student {
+    firstName: string,
+    lastName: string,
+    surName: string,
+    className: string,
+    orgName: string,
+    id: string,
+    avatarId: string
+}
+
+interface Lesson {
+    id: string,
+    lessonid: string,
+    lessonName: string,
+    groupName: string,
+    room: string,
+    number: number,
+    beginHour: number,
+    beginMinute: number,
+    endHour: number,
+    endMinute: number
+}
+
+interface DaySchedule {
+    date: Date,
+    weekdayName: string,
+    lessons: Lesson[]
+}
+
+export async function querySchedule(token: string, studentId: string):Promise<DaySchedule[]> {
+    const resp = await queryEndpoint(`/schedule?studentId=${studentId}`, token);
+    let schedule: DaySchedule[] = [];
+    for (let day of resp.scheduleModel.days) {
+        schedule.push({ 
+            date: day.date,
+            weekdayName: day.dayOfWeekName,
+            lessons: day.scheduleDayLessonModels
+        })
+    }
+    return schedule;
+}
+
+export async function queryStudent(token: string): Promise<Student> {
+    const resp = await queryEndpoint("/students", token);
+    const student: Student = resp.students[0];
+    return student;
+}
+
+async function queryEndpoint(endpoint: string, token: string): Promise<any> {
+    let resp = await fetch(`https://dnevnik.egov66.ru/api${endpoint}`, {
+        method: 'GET',
+        headers: {
+            'Authorization': `Bearer ${token}`,
+            'Cookie': `Aiss2Auth=Bearer%20${token}`
+        },
+    })
+    return await resp.json()
+}
--- src/routes/home/+page.svelte
+++ src/routes/home/+page.svelte
@@ -1,17 +1,40 @@
 <script lang="ts">
 	import { goto } from '$app/navigation';
+	import { onMount } from 'svelte';
 	import type { PageData } from './$types';
+	import { querySchedule, queryStudent } from '$lib/api';
 
 	export let data: PageData;
-
-	function revokeRedir() {
-		goto('/login?revoke');
-	}
+	const token = data.tkn;
 </script>
 
-<div class="flex flex-row">
-	<h1 class="p-5 text-3xl">Welcome! Your token is {data.tkn}</h1>
-	<button class=" m-5 h-10 w-60 rounded-xl bg-slate-600 text-white" on:click={revokeRedir}
-		>Revoke it!</button
-	>
+<div class=" flex w-fit flex-col self-center lg:h-screen lg:flex-wrap">
+	{#await queryStudent(token)}
+		<p>Fetching student...</p>
+	{:then st}
+		{#await querySchedule(token, st.id)}
+			<p>Fetching schedule...</p>
+		{:then sc}
+			{#each sc as d}
+				<div class=" m-3 w-80 rounded-xl bg-white p-3">
+					<p class=" text-center text-slate-700">{d.weekdayName}</p>
+					{#each d.lessons as l}
+						<p class=" w-20">{l.lessonName}</p>
+						<br />
+					{/each}
+				</div>
+			{/each}
+		{:catch esc}
+			<p>Error: {esc}</p>
+		{/await}
+	{:catch es}
+		<p>Error: {es}</p>
+	{/await}
 </div>
+
+<button
+	class=" m-5 h-10 w-60 rounded-xl bg-slate-600 text-white"
+	on:click={() => {
+		goto('/login?revoke');
+	}}>Log out</button
+>
