41afdd0540e4e520ec760a455730b7d55aed5b9a
LaPepega <tmv03092008@gmail.com>
2024-10-15 20:13:08 +0500

    Round R/G indicator component

---
+++ src/lib/components/Indicator.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	export let state: boolean = false;
+	export let size: number = 10;
+	let stateColor: string;
+	let sizePx: string;
+	$: stateColor = state ? '#AAFF00' : '#EE4B2B';
+	$: sizePx = `${size}px`;
+</script>
+
+<svg
+	width={sizePx}
+	height={sizePx}
+	viewBox="0 0 64 64"
+	xmlns="http://www.w3.org/2000/svg"
+	xmlns:xlink="http://www.w3.org/1999/xlink"
+	aria-hidden="true"
+	role="img"
+	class="iconify iconify--emojione"
+	preserveAspectRatio="xMidYMid meet"
+	><circle cx="32" cy="32" r="30" fill={stateColor}></circle></svg
+>
